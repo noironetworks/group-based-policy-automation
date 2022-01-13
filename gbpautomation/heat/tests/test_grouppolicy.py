32dcadeb758f7ef1f59301e1dbe80eebbccb041b
Thomas Bachman <tbachman@yahoo.com>
2022-01-13 17:59:20 +0000

    Python2/3 compatibility fixes
    Results from runnig 2to3 tooling.
    
    Change-Id: I7e029b46a66c3eecbea323c12722d5c5b36076e1
    (cherry picked from commit 21946e6b857e616094d50b88d712baac5dbe527d)

--- gbpautomation/heat/tests/test_grouppolicy.py
+++ gbpautomation/heat/tests/test_grouppolicy.py
@@ -559,7 +559,7 @@ class PolicyTargetTest(HeatTestCase):
                 "description": "test policy target resource",
                 "port_id": "some-port-id",
                 'fixed_ips': [
-                    {'subnet_id': u'test-subnet', 'ip_address': u'10.0.3.21'}
+                    {'subnet_id': 'test-subnet', 'ip_address': '10.0.3.21'}
                 ],
             }
         }
@@ -620,7 +620,7 @@ class PolicyTargetTest(HeatTestCase):
                 "description": "test policy target resource",
                 "port_id": "some-port-id",
                 'fixed_ips': [
-                    {'subnet_id': u'test-subnet', 'ip_address': u'10.0.3.21'}
+                    {'subnet_id': 'test-subnet', 'ip_address': '10.0.3.21'}
                 ],
             }
         }
